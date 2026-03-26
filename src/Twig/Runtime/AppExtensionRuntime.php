854b69e4d24b5da9cd6c240658a1b5dedf7cc6d6
karhel <karheltmarr@hotmail.fr>
2026-03-26 11:14:54 +0100

    Custom Capitalize 1 car sur 2

--- src/Twig/Runtime/AppExtensionRuntime.php
+++ src/Twig/Runtime/AppExtensionRuntime.php
@@ -13,6 +13,30 @@ class AppExtensionRuntime implements RuntimeExtensionInterface
 
     public function customCapitalize($value)
     {
-        return strtoupper($value);
+        /* Méthode #1 : on parcour# tout le tableau, et on check pour chaque index si paire ou impaire
+        for($i = 0; $i < strlen($value); $i++) {
+
+            if($i % 2) {
+                $value[$i] = strtolower($value[$i]);
+            }
+            else {
+                $value[$i] = strtoupper($value[$i]);
+            }
+        }
+
+        return $value;
+        */
+
+        // Méthode #2
+        // Tout en minuscules
+        $value = strtolower($value);
+
+        // Boucle pour tous les nombres pairs
+        for($i = 0; $i < strlen($value); $i = $i + 2) {
+
+            $value[$i] = strtoupper($value[$i]);
+        }
+
+        return $value;
     }
 }
