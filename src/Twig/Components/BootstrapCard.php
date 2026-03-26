6b52219b47463273cc6a787ef30ff312879251e3
karhel <karheltmarr@hotmail.fr>
2026-03-26 13:22:09 +0100

    Bootstrap Card component (ex 1)

---
+++ src/Twig/Components/BootstrapCard.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class BootstrapCard
+{
+    private string $_strTitle;
+    private string $_strImg;
+    private string $_strLink;
+    private string $_strLabel;
+
+    /**
+     * Monte le composant dans le DOM
+     * 
+     * @param string $title Titre de la card
+     * @param string $img l'URL ou l'URI de la source de l'image
+     * @param string $link l'URL ou l'URI du target du lien du bouton
+     * @param string $label Texte affiché dans le bouton
+     */
+    public function mount(string $title, string $img, string $link, string $label): void
+    {
+        $this->_strTitle    = $title;
+        $this->_strImg      = $img;
+        $this->_strLink     = $link;
+        $this->_strLabel    = $label;
+    }
+
+    // GETTERS ================================================================
+
+    public function getTitle(): string
+    {
+        return $this->_strTitle;
+    }
+
+    public function getImg(): string
+    {
+        return $this->_strImg;
+    }
+
+    public function getLink(): string
+    {
+        return $this->_strLink;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->_strLabel;
+    }
+
+
+}
