90c005c9b755eee043b26e75a4a2345d297991a2
karhel <karheltmarr@hotmail.fr>
2026-04-02 14:21:06 +0200

    Création d'un type de pokémon

--- src/Controller/PokemonTypeController.php
+++ src/Controller/PokemonTypeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\PokemonType;
 use App\Form\PokemonTypeCreateFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,10 +17,36 @@ final class PokemonTypeController extends AbstractController
     {
         $objNewType = new PokemonType();
 
-        $createForm = $this->createForm(PokemonTypeCreateFormType::class, $objNewType);
+        // On modifie l'action par défaut (URL vers laquelle les données sont envoyées) du formulaire
+        // $this->generateUrl permet de créer une URL (string) à partir du nom de la route
+        $createForm = $this->createForm(PokemonTypeCreateFormType::class, $objNewType, [
+            'action' => $this->generateUrl('app_type_create')
+        ]);
 
         return $this->render('pokemon_type/index.html.twig', [
             'createForm' => $createForm
         ]);
+    }
+
+    #[Route('/type/create', name: 'app_type_create')]
+    public function handleCreate(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $objNewType = new PokemonType();
+
+        $createForm = $this->createForm(PokemonTypeCreateFormType::class, $objNewType);
+
+        $createForm->handleRequest($request);
+
+        if($createForm->isSubmitted() && $createForm->isValid()) {
+
+            $entityManager->persist($objNewType);
+            $entityManager->flush();
+
+            // Affiche un message de succès
+            $this->addFlash('success', "Le type de pokémon a bien été créé en base");
+        }
+
+        // Redirige vers la liste des types
+        return $this->redirectToRoute('app_type');
     }
 }
