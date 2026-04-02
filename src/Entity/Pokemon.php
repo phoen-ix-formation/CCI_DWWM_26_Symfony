98385bd3551fb75ae3aed577f4614ef36c9d85c3
karhel <karheltmarr@hotmail.fr>
2026-04-02 16:21:30 +0200

    Relation entre Pokémon et Type

--- src/Entity/Pokemon.php
+++ src/Entity/Pokemon.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PokemonRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PokemonRepository::class)]
@@ -19,6 +21,20 @@ class Pokemon
 
     #[ORM\Column(name: 'pkm_number', nullable: true, unique: true)]
     private ?int $number = null;
+
+    /**
+     * @var Collection<int, PokemonType>
+     */
+    #[ORM\JoinTable('pokemon_pokemon_types')]
+    #[ORM\JoinColumn(name: 'ppt_pkm_id', referencedColumnName: 'pkm_id')]
+    #[ORM\InverseJoinColumn(name: 'ppt_pkt_id', referencedColumnName: 'pkt_id')]
+    #[ORM\ManyToMany(targetEntity: PokemonType::class, inversedBy: 'pokemons')]
+    private Collection $types;
+
+    public function __construct()
+    {
+        $this->types = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +61,30 @@ class Pokemon
     public function setNumber(?int $number): static
     {
         $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, PokemonType>
+     */
+    public function getTypes(): Collection
+    {
+        return $this->types;
+    }
+
+    public function addType(PokemonType $type): static
+    {
+        if (!$this->types->contains($type)) {
+            $this->types->add($type);
+        }
+
+        return $this;
+    }
+
+    public function removeType(PokemonType $type): static
+    {
+        $this->types->removeElement($type);
 
         return $this;
     }
--- src/Entity/PokemonType.php
+++ src/Entity/PokemonType.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PokemonTypeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PokemonTypeRepository::class)]
@@ -19,6 +21,17 @@ class PokemonType
 
     #[ORM\Column(name: 'pkt_color', length: 7)]
     private ?string $color = null;
+
+    /**
+     * @var Collection<int, Pokemon>
+     */
+    #[ORM\ManyToMany(targetEntity: Pokemon::class, mappedBy: 'types')]
+    private Collection $pokemons;
+
+    public function __construct()
+    {
+        $this->pokemons = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +58,33 @@ class PokemonType
     public function setColor(string $color): static
     {
         $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Pokemon>
+     */
+    public function getPokemons(): Collection
+    {
+        return $this->pokemons;
+    }
+
+    public function addPokemon(Pokemon $pokemon): static
+    {
+        if (!$this->pokemons->contains($pokemon)) {
+            $this->pokemons->add($pokemon);
+            $pokemon->addType($this);
+        }
+
+        return $this;
+    }
+
+    public function removePokemon(Pokemon $pokemon): static
+    {
+        if ($this->pokemons->removeElement($pokemon)) {
+            $pokemon->removeType($this);
+        }
 
         return $this;
     }
