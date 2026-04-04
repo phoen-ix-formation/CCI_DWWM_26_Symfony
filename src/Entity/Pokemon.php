95fbd5b045e84d0f3602e182bc2ce3cff3c9c64e
Karhel <karheltmarr@hotmail.fr>
2026-04-04 11:23:18 +0200

    Contrainte 1 à 2 types uniquement

--- src/Entity/Pokemon.php
+++ src/Entity/Pokemon.php
@@ -6,6 +6,7 @@ use App\Repository\PokemonRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PokemonRepository::class)]
 #[ORM\Table(name: 'pokemons')]
@@ -25,6 +26,12 @@ class Pokemon
     /**
      * @var Collection<int, PokemonType>
      */
+    #[Assert\Count(
+        min: 1,
+        max: 2,
+        minMessage: 'Un Pokémon doit avoir au moins {{ limit }} type',
+        maxMessage: 'Un Pokémon ne peut pas avoir plus de {{ limit }} types',
+    )]
     #[ORM\JoinTable('pokemon_pokemon_types')]
     #[ORM\JoinColumn(name: 'ppt_pkm_id', referencedColumnName: 'pkm_id')]
     #[ORM\InverseJoinColumn(name: 'ppt_pkt_id', referencedColumnName: 'pkt_id')]
--- src/Form/PokemonCreateFormType.php
+++ src/Form/PokemonCreateFormType.php
@@ -7,6 +7,7 @@ use App\Entity\PokemonType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Validator\Constraints\Count;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -30,7 +31,15 @@ class PokemonCreateFormType extends AbstractType
                 'class'         => PokemonType::class,
                 'choice_label'  => 'name',
                 'multiple'      => true,
-                'label'         => 'Types(s)'
+                'label'         => 'Types(s)',
+                /*'constraints'   => [
+                    new Count(
+                        min: 1,
+                        max: 2,
+                        minMessage: 'Un Pokémon doit avoir au moins {{ limit }} type',
+                        maxMessage: 'Un Pokémon ne peut pas avoir plus de {{ limit }} types',
+                    ),
+                ],*/
             ])
 
             ->add('submit', SubmitType::class, [
