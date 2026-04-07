691f48ff2ad0c89ee8fbcbfa2fb495c45fb43172
karhel <karheltmarr@hotmail.fr>
2026-04-07 16:21:58 +0200

    Afficher les utilisateurs

---
+++ src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $arrUsers = $userRepository->findAll();
+
+        return $this->render('user/index.html.twig', [
+            'userList' => $arrUsers
+        ]);
+    }
+}
