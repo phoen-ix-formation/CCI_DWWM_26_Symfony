b85d236f2432e97b221e0e80ae59299bfb6be15d
Karhel <karheltmarr@hotmail.fr>
2026-04-08 14:15:54 +0200

    Tous les types de Pokémon

--- src/Story/AppStory.php
+++ src/Story/AppStory.php
@@ -29,8 +29,24 @@ final class AppStory extends Story
 
         // Génère tous les types des Pokémons
         PokemonTypeFactory::createSequence([
-            ['name' => "Normal",    'color' => "#FFFFFF"],
-            ['name' => "Feu",       'color' => "#FF0000"]
+            ['name' => "Acier",    'color' => "#60A2B9"],
+            ['name' => "Combat",   'color' => "#FF8100"],
+            ['name' => "Dragon",   'color' => "#4F60E2"],
+            ['name' => "Eau",      'color' => "#2481EF"],
+            ['name' => "Electrik", 'color' => "#FAC100"],
+            ['name' => "Fée",      'color' => "#EF70EF"],
+            ['name' => "Feu",      'color' => "#E72324"],
+            ['name' => "Glace",    'color' => "#3DD9FF"],
+            ['name' => "Insecte",  'color' => "#92A212"],
+            ['name' => "Normal",   'color' => "#A0A2A0"],
+            ['name' => "Plante",   'color' => "#3DA224"],
+            ['name' => "Poison",   'color' => "#923FCC"],
+            ['name' => "Psy",      'color' => "#EF3F7A"],
+            ['name' => "Roche",    'color' => "#B0AA82"],
+            ['name' => "Sol",      'color' => "#92501B"],
+            ['name' => "Spectre",  'color' => "#703F70"],
+            ['name' => "Ténèbre",  'color' => "#4F3F3D"],
+            ['name' => "Vol",      'color' => "#82BAEF"],
         ]);
 
         
