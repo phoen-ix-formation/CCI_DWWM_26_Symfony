37fd0604683d1a6b5a76fa9e41527d51ba527435
Karhel <karheltmarr@hotmail.fr>
2026-04-08 09:42:10 +0200

    User Création et modif (admin)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserInfoFormType;
 use App\Repository\UserRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
@@ -16,6 +22,71 @@ final class UserController extends AbstractController
 
         return $this->render('user/index.html.twig', [
             'userList' => $arrUsers
+        ]);
+    }
+
+    #[Route('/user/create', name: 'app_user_create')]
+    public function create(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $objUser = new User();
+
+        $userForm = $this->createForm(UserInfoFormType::class, $objUser);
+
+        $userForm->handleRequest($request);
+        
+        if($userForm->isSubmitted() && $userForm->isValid()) {
+
+            /** @var string $plainPassword */
+            $plainPassword = $userForm->get('plainPassword')->getData();
+
+            // encode the plain password
+            $objUser->setPassword($userPasswordHasher->hashPassword($objUser, $plainPassword));
+
+            // set the registration datetime
+            $objUser->setRegisteredAt(new DateTimeImmutable('now'));
+            
+            $entityManager->persist($objUser);
+            $entityManager->flush();
+
+            $this->addFlash('success', "L'utilisateur a été créé");
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('user/form.html.twig', [
+            'userForm' => $userForm
+        ]);
+    }
+
+    #[Route('/user/{id<\d+>}', name: 'app_user_update')]
+    public function update(User $user, Request $request, 
+        UserPasswordHasherInterface $userPasswordHasher, 
+        EntityManagerInterface $entityManager): Response
+    {
+        $userForm = $this->createForm(UserInfoFormType::class, $user);
+
+        $userForm->handleRequest($request);
+        
+        if($userForm->isSubmitted() && $userForm->isValid()) {
+
+            /** @var string $plainPassword */
+            $plainPassword = $userForm->get('plainPassword')->getData();
+
+            if($plainPassword !== "") {
+
+                // encode the plain password
+                $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            }
+
+            $entityManager->flush();
+
+            $this->addFlash('success', "L'utilisateur a été modifié");
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('user/form.html.twig', [
+            'userForm' => $userForm
         ]);
     }
 }
--- src/Form/UserInfoFormType.php
+++ src/Form/UserInfoFormType.php
@@ -4,20 +4,19 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
 use Symfony\Component\Validator\Constraints\PasswordStrength;
 
-class RegistrationFormType extends AbstractType
+class UserInfoFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -34,17 +33,6 @@ class RegistrationFormType extends AbstractType
             ->add('birthdate', null, [
                 'label' => 'Date de naissance'
             ])
-
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped'        => false,
-                'label'         => "Accepter les conditions d'utilisation",
-                'constraints'   => [
-                    new IsTrue(
-                        message: 'Vous devez accepter les conditions',
-                    ),
-                ],
-            ])
-
             ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -71,6 +59,9 @@ class RegistrationFormType extends AbstractType
                     new PasswordStrength(),
                     new NotCompromisedPassword(),
                 ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Enregistrer"
             ])
         ;
     }
