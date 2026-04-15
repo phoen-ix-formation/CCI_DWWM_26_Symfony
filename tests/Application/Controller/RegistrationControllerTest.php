ee2e2485d6215ef30decbe30840db5b258cd161f
Karhel <karheltmarr@hotmail.fr>
2026-04-15 14:58:36 +0200

    Test Inscription OK + envoi de mail

--- tests/Application/Controller/RegistrationControllerTest.php
+++ tests/Application/Controller/RegistrationControllerTest.php
@@ -24,6 +24,33 @@ class RegistrationControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
 
         // On complète le formulaire avec des informations correctes
+
+        $client->submitForm("S'inscrire", [
+            'registration_form[email]'                  => "john.doe@email.com",
+            'registration_form[lastname]'               => "Doe",
+            'registration_form[firstname]'              => "John",
+            'registration_form[birthdate]'              => "2000-04-05",
+            'registration_form[plainPassword][first]'   => "M?sth3erbe!!yyyyyyyyy",
+            'registration_form[plainPassword][second]'  => "M?sth3erbe!!yyyyyyyyy",
+            'registration_form[agreeTerms]'             => true,
+        ]);
+
+        $this->assertResponseRedirects();
+
+        // Vérifie qu'un mail est envoyé après l'inscription
+        // Attention à vérifier qu'un mail est envoyé AVANT de follow la redirection
+        // cf. https://symfony.com/doc/current/mailer.html#write-a-functional-test
+        $this->assertEmailCount(1);
+
+        // Récupère le dernier e-mail envoyé
+        $email = $this->getMailerMessage();
+
+        // Test le destinataire et l'objet du mail
+        $this->assertEmailAddressContains($email, 'To', 'john.doe@email.com');
+        $this->assertEmailSubjectContains($email, "Please Confirm your Email");
+
+        $client->followRedirect();
+        $this->assertRouteSame('app_dashboard');
     }
 
     public function testRegisterWithExistingEmail(): void
